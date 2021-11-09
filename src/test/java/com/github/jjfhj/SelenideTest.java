05a040afe3687efa655887730368db2ba9ce2768
Karina <aiwpront@gmail.com>
2021-11-09 23:07:04 +0400

    updated SelenideTest.java

--- src/test/java/com/github/jjfhj/SelenideTest.java
+++ src/test/java/com/github/jjfhj/SelenideTest.java
@@ -1,4 +1,33 @@
 package com.github.jjfhj;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
 public class SelenideTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    public void checkingTheIssueNameInTheRepository() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        //Открыть главную страницу Github
+        open("https://github.com/");
+        //Найти репозиторий jjfhj/qaguru_6
+        $("[name=q]").setValue("jjfhj/qaguru_6").submit();
+        //Перейти в найденный репозиторий
+        $(linkText("jjfhj/qaguru_6")).click();
+        //Найти название Issues в репозитории
+        $("#issues-tab").shouldHave(text("Issues"));
+    }
 }

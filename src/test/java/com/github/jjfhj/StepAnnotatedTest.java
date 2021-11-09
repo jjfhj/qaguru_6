ba7b6c09ad5f23bffd7eccd20af3891e4ee6f679
Karina <aiwpront@gmail.com>
2021-11-10 00:23:36 +0400

    updated StepAnnotatedTest.java

--- src/test/java/com/github/jjfhj/StepAnnotatedTest.java
+++ src/test/java/com/github/jjfhj/StepAnnotatedTest.java
@@ -1,4 +1,25 @@
 package com.github.jjfhj;
 
+import com.codeborne.selenide.Configuration;
+import com.github.jjfhj.steps.WebSteps;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class StepAnnotatedTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    private static final String REPOSITORY = "jjfhj/qaguru_6";
+
+    private WebSteps steps = new WebSteps();
+
+    @Test
+    public void checkingTheIssueNameInTheRepository() {
+        steps.openMainPage();
+        steps.searchForRepository(REPOSITORY);
+        steps.goToRepository(REPOSITORY);
+        steps.findIssuesTitle(REPOSITORY);
+    }
 }
